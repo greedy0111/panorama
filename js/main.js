ed8a787df862b8a9980ae4238367838bdcd1a037
Hyunjun Kim <45992258+greedy0111@users.noreply.github.com>
2025-10-20 05:46:03 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function () {
+    const circle = document.querySelector('#circle');
+
+    const article = document.querySelectorAll('article');
+    
+    for (let el of article) {
+        el.addEventListener('mouseenter', e => {
+            circle.style.animationPlayState = 'paused';
+        })
+        el.addEventListener('mouseleave', e => {
+            circle.style.animationPlayState = 'running';
+        })
+    }
+})();
